39960a8c4f55b989f56bd20020b01d6139c44039
Dilip Reddy Mavuram <dilip426426@gmail.com>
2020-01-06 15:59:54 +0100

    Pass arguments to Docker build

--- build_yocto_16.04.sh
+++ build_yocto_16.04.sh
@@ -2,4 +2,4 @@
 
 docker --version
 
-docker build -t yocto_16.04 -f yocto_16.04 .
+docker build -t yocto_16.04 -f yocto_16.04 --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) .
